a65fdfbf095382938426d77beab8d2bac50bb61c
Matthew the GREAT <mctabor@gmail.com>
2014-05-27 11:43:50 -0700

    Logic cleanup
    Able to overwrite original jpeg image after rotation now.

--- AutoRotateImages/Program.cs
+++ AutoRotateImages/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 using System.Drawing;
 using System.Drawing.Imaging;
@@ -15,9 +12,9 @@ namespace AutoRotateImages
         {
             string path = args[0];
             //string path = @"C:\Users\McTabor\Pictures\Emmett Samples";
-            var files = Directory.GetFiles(path, "*.jpg");
+            var files = Directory.EnumerateFiles(path).Where(f => f.EndsWith(".jpg") || f.EndsWith(".jpeg") || f.EndsWith("JPG") || f.EndsWith("JPEG")).ToList();
 
-            if (files.Length < 1)
+            if (files.Count < 1)
             {
                 Console.WriteLine("No .jpg images found in {0}.\nPress enter to exit.", path);
                 Console.Read();
@@ -26,69 +23,79 @@ namespace AutoRotateImages
 
             foreach (var item in files)
             {
-                bool edited = false;
+                //System.IO.FileStream fs = new System.IO.FileStream(item, System.IO.FileMode.Open);
                 FileStream fs = new FileStream(item, FileMode.Open);
-                Image im = Image.FromStream(fs);
-                ImageFormat sourceFormat = im.RawFormat;
-                EncoderParameters encoderParameters = null;
-                PropertyItem pi = im.GetPropertyItem(274);
+                Image img = Image.FromStream(fs);
+                PropertyItem pi = img.GetPropertyItem(274);
                 int orientation = pi.Value[0];
-
-                encoderParameters = new EncoderParameters(1);
+                EncoderParameters encoderParameters = new EncoderParameters(1);
+                ImageFormat sourceFormat = img.RawFormat;
+                MemoryStream ms;
+                byte[] ni;
 
                 switch (orientation)
                 {
-                    case 1:
-                        break;
-                    case 2:
-                        
-                        break;
-                    case 3:
-                       
-                        break;
-                    case 4:
-                        break;
-                    case 5:
-                        break;
                     case 6:
                         encoderParameters.Param[0] = new EncoderParameter(System.Drawing.Imaging.Encoder.Transformation, (long)EncoderValue.TransformRotate90);
-                        edited = true;
+
+                        // Set orientation to normal so if we run this again it doesn't rotate an already rotated image
                         pi.Value[0] = 1;
-                        im.SetPropertyItem(pi);
-                        break;
-                    case 7:
+                        img.SetPropertyItem(pi);
+
+                        // Added new memorystream
+                        ms = new MemoryStream();
+
+                        // Read into memorystream
+                        img.Save(ms, GetEncoder(sourceFormat), encoderParameters);
+                        fs.Close();
+                        File.Delete(item);
+
+                        // Read from memorystream into byte array
+                        ni = ms.ToArray();
+
+                        // Save out using new filestream.
+                        using (FileStream nfs = new FileStream(item, FileMode.Create, FileAccess.ReadWrite))
+                        {
+                            nfs.Write(ni, 0, ni.Length);
+                        }
+
                         break;
                     case 8:
-                        encoderParameters.Param[0] = new EncoderParameter(System.Drawing.Imaging.Encoder.Transformation, (long)EncoderValue.TransformRotate270);
-                        edited = true;
+                        encoderParameters.Param[0] = new System.Drawing.Imaging.EncoderParameter(System.Drawing.Imaging.Encoder.Transformation, (long)System.Drawing.Imaging.EncoderValue.TransformRotate270);
+                        
+                        // Set orientation to normal so if we run this again it doesn't rotate an already rotated image
                         pi.Value[0] = 1;
-                        im.SetPropertyItem(pi);
+                        img.SetPropertyItem(pi);
+                        
+                        // Added new memorystream
+                        ms = new MemoryStream();
+
+                        // Read into memorystream
+                        img.Save(ms, GetEncoder(sourceFormat), encoderParameters);
+                        fs.Close();
+                        File.Delete(item);
+
+                        // Read from memorystream into byte array
+                        ni = ms.ToArray();
+
+                        // Save out using new filestream.
+                        using (FileStream nfs = new FileStream(item, FileMode.Create, FileAccess.ReadWrite))
+                        {
+                            nfs.Write(ni, 0, ni.Length);
+                        }
                         break;
                 }
 
-                if (edited)
-                {
-                    string newpath = path + "\\" + Path.GetFileNameWithoutExtension(item) + "-1" + ".jpg";
-                    im.Save(newpath, GetEncoder(sourceFormat), encoderParameters);
-                    fs.Close();
-                    File.Delete(item);
-                }
 
-                im.Dispose();
-                if (encoderParameters != null)
-                    encoderParameters.Dispose();
-                fs.Dispose();
-            }
-
-            // TODO: Why can't I overwrite files above???
-            files = Directory.GetFiles(path, "*-1.jpg");
-            foreach (var f in files)
-            {
-                File.Move(f, f.Replace("-1", ""));
             }
         }
 
-        private static ImageCodecInfo GetEncoder(ImageFormat format)
+        public static void SaveJpeg(string path, Image img, EncoderParameters encoderParameters)
+        {
+            
+        }
+
+        public static ImageCodecInfo GetEncoder(ImageFormat format)
         {
             foreach (var info in ImageCodecInfo.GetImageEncoders())
                 if (info.FormatID == format.Guid)
